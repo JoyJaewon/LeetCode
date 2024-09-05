73de3d06159a6e0b789a2681c9ad55849c68f51e
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-09-05 05:46:30 -0500

    Time: 492 ms (32.23%), Space: 29.6 MB (69.39%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,18 +1,26 @@
 '''
-5th largest
-python heapify - min heap
-[3,2,1,5,6,4] => [-6, -5, -4, -3, -2, -1], k = 2
+input: list, inter k
+output: integer - kth largest element in the array
 
+Clarification
+- can there be duplicate numbers in the list? yes
+- can there be negative numbers in the list? yes
+- do we count duplicate numbers when considering the largest element? yes
 
+Test Cases
+[3,2,1,5,6,4], 2 -> 5
+[6,5,5,4,3], 3 -> 5
+
+Approach) use heap 
 '''
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums = [-num for num in nums]
-        heapq.heapify(nums)
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
 
-        for _ in range(k - 1):
-            heapq.heappop(nums)
-        
-        return -nums[0] 
+        return heapq.heappop(heap)
         
