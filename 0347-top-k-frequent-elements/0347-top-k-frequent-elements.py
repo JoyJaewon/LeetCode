86668d043f4cf79940ebb7989e1f92672fe5481d
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-10-01 16:12:05 -0500

    Time: 73 ms (99.12%), Space: 21.4 MB (31.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,30 @@
-import heapq
+import random
 from collections import Counter
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums) # {num, frequency}
-        heap = []
-
-        for key, val in count.items():
-            heapq.heappush(heap, (val, key))
-            while len(heap) > k:
-                heapq.heappop(heap)
+    def topKFrequent(self, nums, k):
+        count = Counter(nums)
+        unique = list(count.keys())
         
-        return [key for val, key in heap]
+        def quick_select(unique, k):
+            pivot = random.choice(unique)
+            left, mid, right = [], [], []
 
-        
+            for num in unique:
+                if count[num] > count[pivot]:
+                    left.append(num)
+                elif count[num] < count[pivot]:
+                    right.append(num)
+                else:
+                    mid.append(num)
+
+            if k <= len(left):
+                return quick_select(left, k)
+
+            if len(left) + len(mid) == k:
+                return left + mid
+
+            if len(left) + len(mid) < k:
+                return left + mid + quick_select(right, k - len(left) - len(mid))
+
+        return quick_select(unique, k)
