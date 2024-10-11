18276a463833151ee6f40879c486538375df5f5a
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-10-11 14:26:41 -0500

    Time: 89 ms (73.72%), Space: 16.5 MB (58.81%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,29 @@
+'''
+check row, col, sub_box
+using defaultdict(set) 
+'''
+from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        sub_box = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                
+                if (board[r][c] in row[r]
+                    or board[r][c] in col[c]
+                    or board[r][c] in sub_box[(r//3, c//3)]
+                ): return False
+
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                sub_box[(r//3, c//3)].add(board[r][c])
+        
+        return True
+
+
+        
