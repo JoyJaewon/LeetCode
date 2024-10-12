8af827b7ce6bb0be11207f3c1d88b3df83f19289
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-10-11 23:25:44 -0500

    Time: 47 ms (91.95%), Space: 16.7 MB (33.56%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,41 +1,24 @@
-'''
-input: 2 strings
-output: boolean - true if s1's permutation is in s2
-
-Test Cases
-'ab', 'eidbaooo' -> true
-
-if len(s1) > len(s2): False
-create count dictionary for s1
-window length = s1 length. 
-'''
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
         
-        s1_count, s2_count = {}, {}
+        count_s1 = [0] * 26
+        count_s2 = [0] * 26 
 
         for i in range(len(s1)):
-            s1_count[s1[i]] = s1_count.get(s1[i], 0) + 1
-            s2_count[s2[i]] = s2_count.get(s2[i], 0) + 1
-        
-        if s1_count == s2_count:
+            count_s1[ord(s1[i]) - ord('a')] += 1
+            count_s2[ord(s2[i]) - ord('a')] += 1
+                
+        if count_s1 == count_s2:
             return True
         
         left = 0
         for right in range(len(s1), len(s2)):
-            s2_count[s2[right]] = s2_count.get(s2[right], 0) + 1
-            s2_count[s2[left]] -= 1
-            if s2_count[s2[left]] == 0:
-                del s2_count[s2[left]] 
-                
-            if s1_count == s2_count:
+            count_s2[ord(s2[right]) - ord('a')] += 1
+            count_s2[ord(s2[left]) - ord('a')] -= 1
+            if count_s1 == count_s2:
                 return True
             left += 1
         
         return False
-        
-        
-
-        
