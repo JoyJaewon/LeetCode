24d63726305374d000afbe437c42ab694796c89b
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-10-11 21:42:46 -0500

    Time: 46 ms (92.97%), Space: 20.9 MB (19.49%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,13 +2,13 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        
 
     def push(self, val: int) -> None:
         if not self.stack:
             self.stack.append((val, val))
         else:
             self.stack.append((val, min(self.stack[-1][1], val)))
+
         
 
     def pop(self) -> None:
