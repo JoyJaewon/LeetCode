d10b9359dfc09e7af2dea3a7617be11b9f0dd6e0
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-10-22 09:14:13 -0500

    Time: 11 ms (99.96%), Space: 16.7 MB (71.67%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,22 +3,24 @@ class Solution:
         if len(s1) > len(s2):
             return False
         
-        count_s1 = [0] * 26
-        count_s2 = [0] * 26 
+        s1_count = [0] * 26
+        s2_count = [0] * 26
 
         for i in range(len(s1)):
-            count_s1[ord(s1[i]) - ord('a')] += 1
-            count_s2[ord(s2[i]) - ord('a')] += 1
-                
-        if count_s1 == count_s2:
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        if s1_count == s2_count:
             return True
         
         left = 0
         for right in range(len(s1), len(s2)):
-            count_s2[ord(s2[right]) - ord('a')] += 1
-            count_s2[ord(s2[left]) - ord('a')] -= 1
-            if count_s1 == count_s2:
+            s2_count[ord(s2[right]) - ord('a')] += 1
+            s2_count[ord(s2[left]) - ord('a')] -= 1
+            if s1_count == s2_count:
                 return True
+            
             left += 1
         
         return False
+        
