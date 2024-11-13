a5bf0c81aa5def7398348a14fefa60334b35a96b
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-11-13 15:17:12 -0600

    Time: 7 ms (54.73%), Space: 20.3 MB (69.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,30 +1,30 @@
-import random
+'''
+clarifications
+- can have duplicate elements in the array
+
+test cases
+[1,1,1,2,2,3], k=2 => 1:3, 2:2, 3;1 => [1,2]
+
+plan
+- count the frequency  # key: element, value: count
+- going through the dictionary with count
+    - heap
+    - 카운트 큰것만 킵
+
+TC: O(nlogn), SC: O(N)
+'''
+import heapq
 from collections import Counter
-
 class Solution:
-    def topKFrequent(self, nums, k):
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
-        unique = list(count.keys())
+        heap = []
+
+        for key, values in count.items():
+            heapq.heappush(heap, (values, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
-        def quick_select(unique, k):
-            pivot = random.choice(unique)
-            left, mid, right = [], [], []
+        return [value for _, value in heap]
 
-            for num in unique:
-                if count[num] > count[pivot]:
-                    left.append(num)
-                elif count[num] < count[pivot]:
-                    right.append(num)
-                else:
-                    mid.append(num)
-
-            if k <= len(left):
-                return quick_select(left, k)
-
-            if len(left) + len(mid) == k:
-                return left + mid
-
-            if len(left) + len(mid) < k:
-                return left + mid + quick_select(right, k - len(left) - len(mid))
-
-        return quick_select(unique, k)
+        
