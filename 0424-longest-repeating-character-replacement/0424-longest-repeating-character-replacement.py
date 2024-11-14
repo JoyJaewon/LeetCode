dc07b1bbae3db6d76d0b7715bc84285f1a564a16
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-11-13 20:18:28 -0600

    Time: 81 ms (35.76%), Space: 16.7 MB (63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,25 @@
+'''
+use dictionary to track the frequency in the window
+aababba, 2
+l
+   r
+{a:3, b:1} 
+length - max(dict.values())
+5 - 3 = 2
+'''
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        char_dict = {}
-        left = 0
         longest = 0
+        count = {}
+        left = 0
 
         for right in range(len(s)):
-            char_dict[s[right]] = char_dict.get(s[right], 0) + 1
-
-            while (right - left + 1) - max(char_dict.values()) > k:
-                char_dict[s[left]] -= 1
+            count[s[right]] = count.get(s[right], 0) + 1
+            while (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
                 left += 1
             longest = max(longest, right - left + 1)
-        
+
         return longest
+
+        
