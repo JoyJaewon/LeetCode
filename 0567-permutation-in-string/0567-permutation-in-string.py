2f89fc616c0bb1dbf047c78133a560ac1a66daac
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2024-11-13 20:26:43 -0600

    Time: 11 ms (97.59%), Space: 16.7 MB (73.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,26 +1,27 @@
+'''
+true if s2 contains a permutation of s1
+s2 = xxxxxxxxxxx
+s1 = xxxx
+'''
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s2) < len(s1):
             return False
-        
-        s1_count = [0] * 26
-        s2_count = [0] * 26
+        count_s1 = [0] * 26
+        count_s2 = [0] * 26
 
         for i in range(len(s1)):
-            s1_count[ord(s1[i]) - ord('a')] += 1
-            s2_count[ord(s2[i]) - ord('a')] += 1
+            count_s1[ord(s1[i]) - ord('a')] += 1
+            count_s2[ord(s2[i]) - ord('a')] += 1
         
-        if s1_count == s2_count:
+        if count_s1 == count_s2:
             return True
         
         left = 0
         for right in range(len(s1), len(s2)):
-            s2_count[ord(s2[right]) - ord('a')] += 1
-            s2_count[ord(s2[left]) - ord('a')] -= 1
-            if s1_count == s2_count:
+            count_s2[ord(s2[right]) - ord('a')] += 1
+            count_s2[ord(s2[left]) - ord('a')] -= 1
+            if count_s1 == count_s2:
                 return True
-            
             left += 1
-        
         return False
-        
