21a30c24e124eae6b4c363c6710a917e51bd5e92
Jaewon Han <38743869+JoyJaewon@users.noreply.github.com>
2026-06-05 00:10:04 -0500

    Time: 0 ms (100%), Space: 19.5 MB (19.46%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.py
@@ -0,0 +1,33 @@
+'''
+- output: index of the array (if multiple answer, return the smallest)
+- "Minimum" capacity
+- return -1 if no answer
+
+- [1,5,3,7], 3
+1 -> x, 5-> o, 3-> o, 7-> o
+=> 5, 3, 7 are possible answers. since 3 is the smallest capacity, return 2(index)
+
+- initialize val, index
+- iterate through the array
+    - if curr_val >= itemSize and curr_val < val, val = curr_val
+    - index = curr_index
+- if val = inf, return -1
+- else return index
+
+TC: O(N), SC: O(1)
+
+
+'''
+class Solution:
+    def minimumIndex(self, capacity: list[int], itemSize: int) -> int:
+        val = float('inf')
+        index = -1
+
+        for i in range(len(capacity)):
+            if capacity[i] >= itemSize and capacity[i] < val:
+                val = capacity[i]
+                index = i
+        if val == float('inf'):
+            return -1
+        return index
+        
